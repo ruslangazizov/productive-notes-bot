7cb6167c330859d71f995d046632904fccd5b0a3
Ruslan <ruslangazizov36@gmail.com>
2021-08-09 18:05:34 +0300

    Adjust user telegram id handling in menu transitions

--- handlers/users/update_categories.py
+++ handlers/users/update_categories.py
@@ -1,8 +1,9 @@
-from typing import List
+from typing import List, Union
 
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
+from aiogram.types import CallbackQuery
 
 from handlers.users.main_menu import get_and_save_category, get_category
 from handlers.users.start import reset_state_saving_user_id
@@ -15,10 +16,13 @@ from states.categories import DeleteCategory, AddCategory
 
 
 @dp.message_handler(Text(equals="Категории"))
-async def show_all_categories(message: types.Message, state: FSMContext, show_info=True):
+async def show_all_categories(message: Union[types.Message, CallbackQuery], state: FSMContext, show_info=True):
     message_text = "Ваши категории:"
-    user_tg_id = message.from_user.id
-    await state.update_data(user_tg_id=user_tg_id)
+    if isinstance(message, types.Message):
+        user_tg_id = message.from_user.id
+        await state.update_data(user_tg_id=user_tg_id)
+    else:
+        user_tg_id = (await state.get_data()).get("user_tg_id")
 
     user_categories_lst: List[str] = await db.get_user_categories(user_tg_id)
     for index, category in enumerate(user_categories_lst):
@@ -26,6 +30,8 @@ async def show_all_categories(message: types.Message, state: FSMContext, show_in
         count_of_notes_in_category = len(notes_in_category)
         message_text += f"\n{index + 1}. <b>{category}</b> (кол-во заметок: {count_of_notes_in_category})"
 
+    if isinstance(message, CallbackQuery):
+        message = message.message
     if show_info:
         await message.answer("Чтобы вернуться в главное меню, нажмите <b>Главное меню</b>",
                              reply_markup=back_menu)
@@ -69,7 +75,7 @@ async def delete_category(call: types.CallbackQuery, state: FSMContext):
                            state=DeleteCategory.delete_confirmation)
 async def deny_delete_category(call: types.CallbackQuery, state: FSMContext):
     await reset_state_saving_user_id(state)
-    await show_all_categories(call.message, state, False)
+    await show_all_categories(call, state, False)
 
 
 @dp.callback_query_handler(confirmation_callback.filter(action_name="add"))
